c01bf045fd98414f8ee1ff49fad7d769d9fd1a69
Asjadamir <123183132+Asjadamir@users.noreply.github.com>
2026-06-12 01:01:10 +0500

    added vercel analytics and insights

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,18 @@
-import { StrictMode } from "react";
+//import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "@/store/index.js";
 import "./index.css";
 import App from "./App.jsx";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")).render(
-    <StrictMode>
+    <>
         <Provider store={store}>
             <App />
+            <Analytics />
+            <SpeedInsights />
         </Provider>
-    </StrictMode>,
+    </>,
 );
